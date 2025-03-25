1dc4f785b5ed7ec796d97ed925801069b29e02a9
Jean Christoffer <89041011+Jean-Christoffer@users.noreply.github.com>
2025-03-25 21:43:31 +0100

    refactor: description

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -2,10 +2,11 @@ export default function About() {
   return (
     <article className="relative z-10 max-w-2xl mt-4 animate5 opacity-0">
       <p className="text-base">
-        Experienced developer working with TypeScript, specializing in React and
-        Next.js. I have a strong passion for frontend development, crafting
-        engaging UIs with a focus on accessibility. While my expertise is in
-        frontend, my professional career has been in full-stack web development.
+        Experienced developer specializing in React, Next.js, and TypeScript. I
+        have a strong passion for frontend development, creating engaging user
+        interfaces with a focus on design and accessibility. Although my primary
+        expertise is in frontend, my professional experience spans full-stack
+        web development.
       </p>
     </article>
   );
