bdb0d57b1c43f3d1aa1b3c0c9be1ce6f048aa341
Jean Christoffer Dahl <jeanchristofferdahl@Jean-sin-MacBook-Pro.local>
2025-07-15 13:09:13 +0200

    feat: interactive bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Roboto } from "next/font/google";
+import Cursor from "./components/Cursor";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${roboto.className} antialiased`}
       >
+        <Cursor />
         {children}
       </body>
     </html>
